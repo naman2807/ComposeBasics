054efa6df1b87e6004548ef1497476038e483b51
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-01 14:06:30 +0530

    Called Greeting() composable in for loop

--- app/src/main/java/com/example/composebasics/MainActivity.kt
+++ app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -45,7 +45,7 @@ fun MyApp(content: @Composable () -> Unit){
 fun MyScreenContent(names: List<String> = listOf("Android", "There")){
     Column{
        for(name in names){
-
+            Greeting(name = name)
        }
     }
 }
