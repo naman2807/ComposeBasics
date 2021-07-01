6d163ea07cb26ae491a5c3a76398229e10b6c52a
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-01 16:03:59 +0530

    Added color to Divider in MyScreenContent() composable

--- app/src/main/java/com/example/composebasics/MainActivity.kt
+++ app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -60,7 +60,7 @@ fun MyScreenContent(names: List<String> = listOf("Android", "There")){
             Greeting(name = name)
            Divider(color = Color.Black)
        }
-        Divider()
+        Divider(color = Color.Transparent)
     }
 }
 
